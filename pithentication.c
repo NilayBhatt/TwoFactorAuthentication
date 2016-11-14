b913d87f9b3c4ef1ad3887348fec478a6532795a
edmiralagic <edmir.alagic@my.ccsu.edu>
2016-11-14 10:07:05 -0500

    Added checkpoints in the C file

---
+++ pithentication.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+//include sql
+//include email server
+
+//variable for user session id -- initialized to null
+//bunch of arrays to store notes temporarily
+
+//method for login
+//method for sign up
+//method for db connection
+//method for sending email/sms text message to specified email/number with random key
+//method to check both factors of authentication and establish a session id
+//method to display user page
+//methods for the various note commands -- create -- delete -- filter -- edit -- etc
+//method to sign out
+
+int main(int argc, char *argv[]){
+	//print introductory message
+	//switch statement for scanf -- either login or sign up
+
+	//once logged in/signed up -- show user page -- switch statements for various command
+	//create note command
+	//delete note command
+	//edit note command
+	//order of notes
+	//show more for certain note
+	//etc
+	
+	//command for signing out of session
+	return 0;
+}
